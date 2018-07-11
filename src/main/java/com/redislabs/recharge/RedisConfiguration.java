8f5e67499332b6f997fd8983b959a98c6bd7be1f
jruaux <jruaux@jrx.home.ruaux.org>
2018-07-11 11:07:58 -0700

    removed jedis connection factory

--- src/main/java/com/redislabs/recharge/RedisConfiguration.java
+++ src/main/java/com/redislabs/recharge/RedisConfiguration.java
@@ -3,16 +3,10 @@ package com.redislabs.recharge;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 @EnableAutoConfiguration
 public class RedisConfiguration {
-
-	@Bean
-	public RedisConnectionFactory connectionFactory() {
-		return new JedisConnectionFactory();
-	}
 
 	@Bean
 	public StringRedisTemplate redisTemplate(RedisConnectionFactory connectionFactory) {
