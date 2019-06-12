24e5522c1600ecb9402ed207e4a2bd0f50d45889
Julien Ruaux <jruaux@gmail.com>
2019-06-11 19:01:44 -0700

    fixed completion message

--- src/main/java/com/redislabs/riot/cli/JobCommand.java
+++ src/main/java/com/redislabs/riot/cli/JobCommand.java
@@ -72,8 +72,8 @@ public class JobCommand<I, O> extends AbstractCommand {
 						.ofMillis(stepExecution.getEndTime().getTime() - stepExecution.getStartTime().getTime());
 				int writeCount = stepExecution.getWriteCount();
 				double throughput = (double) writeCount / duration.toMillis() * 1000;
-				System.out.println(stepExecution.getStepName() + ": " + numberFormat.format(writeCount) + " items in "
-						+ duration.toSeconds() + " seconds (" + numberFormat.format(throughput) + " items/sec)");
+				System.out.println("Wrote " + numberFormat.format(writeCount) + " items in " + duration.toSeconds()
+						+ " seconds (" + numberFormat.format(throughput) + " items/sec)");
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
