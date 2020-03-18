c0724bf78257c006610be987c64aa3e54e368e9a
Julien Ruaux <jruaux@gmail.com>
2020-03-18 01:51:48 -0700

    changed file input option

--- src/main/java/com/redislabs/riot/cli/file/FileOptions.java
+++ src/main/java/com/redislabs/riot/cli/file/FileOptions.java
@@ -1,6 +1,8 @@
 package com.redislabs.riot.cli.file;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
@@ -10,26 +12,28 @@ import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
-import org.springframework.util.ResourceUtils;
 
 import com.redislabs.riot.file.StandardInputResource;
 import com.redislabs.riot.file.StandardOutputResource;
 
+import picocli.CommandLine.ArgGroup;
 import picocli.CommandLine.Option;
 
 public class FileOptions {
 
 	private final static String EXTENSION_GROUP = "extension";
+	private final static String GZ_GROUP = "gz";
 	private final static String EXTENSION_CSV = "csv";
 	private final static String EXTENSION_TSV = "tsv";
 	private final static String EXTENSION_DAT = "dat";
 	private final static String EXTENSION_JSON = "json";
 	private final static String EXTENSION_XML = "xml";
-	private final static Pattern EXTENSION = Pattern.compile("(?i).*\\.(?<" + EXTENSION_GROUP + ">" + EXTENSION_CSV
-			+ "|" + EXTENSION_DAT + "|" + EXTENSION_JSON + "|" + EXTENSION_TSV + "|" + EXTENSION_XML + ")(\\.gz)?$");
+	private final static Pattern EXTENSION = Pattern
+			.compile("(?i).*\\.(?<" + EXTENSION_GROUP + ">" + EXTENSION_CSV + "|" + EXTENSION_DAT + "|" + EXTENSION_JSON
+					+ "|" + EXTENSION_TSV + "|" + EXTENSION_XML + ")(?<" + GZ_GROUP + ">\\.gz)?$");
 
 	private final static Map<String, FileType> FILETYPES = new HashMap<>();
+
 	static {
 		FILETYPES.put(EXTENSION_CSV, FileType.CSV);
 		FILETYPES.put(EXTENSION_DAT, FileType.CSV);
@@ -38,8 +42,15 @@ public class FileOptions {
 		FILETYPES.put(EXTENSION_XML, FileType.XML);
 	}
 
-	@Option(names = { "-f", "--file" }, description = "File path or URL")
-	private String file;
+	static class PathOptions {
+		@Option(names = { "-f", "--file" }, description = "Path to local file")
+		private File file;
+		@Option(names = "--url", description = "URL of a file")
+		private URI uri;
+	}
+
+	@ArgGroup(exclusive = true, multiplicity = "1")
+	private PathOptions path = new PathOptions();
 	@Option(names = { "-z", "--gzip" }, description = "File is gzip compressed")
 	private boolean gzip;
 	@Option(names = { "-t", "--filetype" }, description = "File type: ${COMPLETION-CANDIDATES}", paramLabel = "<type>")
@@ -52,8 +63,7 @@ public class FileOptions {
 	private String region;
 	@Option(names = "--fields", arity = "1..*", description = "Field names", paramLabel = "<names>")
 	private String[] names = new String[0];
-	@Option(names = { "-e",
-			"--encoding" }, description = "File encoding (default: ${DEFAULT-VALUE})", paramLabel = "<charset>")
+	@Option(names = "--encoding", description = "File encoding (default: ${DEFAULT-VALUE})", paramLabel = "<charset>")
 	private String encoding = FlatFileItemReader.DEFAULT_CHARSET;
 	@Option(names = { "-h", "--header" }, description = "First line contains field names")
 	private boolean header;
@@ -92,7 +102,7 @@ public class FileOptions {
 	}
 
 	private boolean isConsoleResource() {
-		return file.equals("-");
+		return path.file != null && path.file.equals("-");
 	}
 
 	protected Resource getOutputResource() throws IOException {
@@ -103,27 +113,46 @@ public class FileOptions {
 	}
 
 	private Resource getResource() throws IOException {
-		if (ResourceUtils.isUrl(file)) {
-			UrlResource resource = new UrlResource(file.toString());
-			if (resource.getURI().getScheme().equals("s3")) {
-				return S3ResourceBuilder.resource(accessKey, secretKey, region, resource.getURI());
-			}
-			return new UncustomizedUrlResource(resource.getURI());
+		if (path.file != null) {
+			return new FileSystemResource(path.file);
 		}
-		return new FileSystemResource(file);
+		if (path.uri != null) {
+			if ("s3".equals(path.uri.getScheme())) {
+				return S3ResourceBuilder.resource(accessKey, secretKey, region, path.uri);
+			}
+			return new UncustomizedUrlResource(path.uri);
+		}
+		throw new IllegalArgumentException("No file or URL specified");
 	}
 
 	protected boolean isGzip() {
-		return gzip || file.toLowerCase().endsWith(".gz");
+		if (gzip) {
+			return true;
+		}
+		Matcher matcher = EXTENSION.matcher(filename());
+		if (matcher.matches()) {
+			return matcher.group(GZ_GROUP) != null;
+		}
+		return false;
+	}
+
+	private String filename() {
+		if (path.file != null) {
+			return path.file.getName();
+		}
+		if (path.uri != null) {
+			return path.uri.toString();
+		}
+		throw new IllegalArgumentException("No file or URL specified");
 	}
 
 	public boolean isSet() {
-		return file != null;
+		return path.file != null || path.uri != null;
 	}
 
 	protected FileType getType() {
 		if (type == null) {
-			Matcher matcher = EXTENSION.matcher(file);
+			Matcher matcher = EXTENSION.matcher(filename());
 			if (matcher.matches()) {
 				return FILETYPES.get(matcher.group(EXTENSION_GROUP));
 			}
