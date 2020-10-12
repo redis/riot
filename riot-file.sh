cf461ebc47ad1beb4f29f22de37e6214d30c828d
jruaux <jruaux@gmail.com>
2020-10-12 14:31:33 -0700

    moved each run script to respective connector updated docs

--- riot-file.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh -e
-
-gradle -q --console plain installDist
-
-./riot-file/build/install/riot-file/bin/riot-file "$@"
--- riot-redis.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh -e
-
-gradle -q --console plain installDist
-
-./riot-redis/build/install/riot-redis/bin/riot-redis "$@"
