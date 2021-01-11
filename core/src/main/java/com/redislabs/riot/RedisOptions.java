c2f0ffc951159447c09d4da25edcf5938062a90a
jruaux <jruaux@gmail.com>
2021-01-11 00:04:13 -0800

    made options negatable when applicable

--- core/src/main/java/com/redislabs/riot/RedisOptions.java
+++ core/src/main/java/com/redislabs/riot/RedisOptions.java
@@ -20,6 +20,8 @@ import picocli.CommandLine.Option;
 
 import java.io.File;
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 public class RedisOptions {
@@ -30,73 +32,74 @@ public class RedisOptions {
     public static final int DEFAULT_TIMEOUT = 60;
     public static final int DEFAULT_POOL_MAX_TOTAL = 8;
 
-    @Option(names = {"-h", "--hostname"}, description = "Server hostname (default: ${DEFAULT-VALUE})", paramLabel = "<host>")
+    @Option(names = {"-h", "--hostname"}, description = "Server hostname (default: ${DEFAULT-VALUE}).", paramLabel = "<host>")
     private String host = DEFAULT_HOST;
-    @Option(names = {"-p", "--port"}, description = "Server port (default: ${DEFAULT-VALUE})", paramLabel = "<port>")
+    @Option(names = {"-p", "--port"}, description = "Server port (default: ${DEFAULT-VALUE}).", paramLabel = "<port>")
     private int port = DEFAULT_PORT;
-    @Option(names = {"-s", "--socket"}, description = "Server socket (overrides hostname and port)", paramLabel = "<socket>")
+    @Option(names = {"-s", "--socket"}, description = "Server socket (overrides hostname and port).", paramLabel = "<socket>")
     private String socket;
     @Option(names = "--user", description = "Used to send ACL style 'AUTH username pass'. Needs password.", paramLabel = "<username>")
     private String username;
-    @Option(names = {"-a", "--pass"}, arity = "0..1", interactive = true, description = "Password to use when connecting to the server", paramLabel = "<password>")
+    @Option(names = {"-a", "--pass"}, arity = "0..1", interactive = true, description = "Password to use when connecting to the server.", paramLabel = "<password>")
     private char[] password;
-    @Option(names = {"-u", "--uri"}, description = "Server URI", paramLabel = "<uri>")
-    private RedisURI redisURI;
-    @Option(names = {"-o", "--timeout"}, description = "Redis command timeout (default: ${DEFAULT-VALUE})", paramLabel = "<sec>")
+    @Option(names = {"-u", "--uri"}, arity = "0..*", description = "Server URI.", paramLabel = "<uri>")
+    private List<RedisURI> uris = new ArrayList<>();
+    @Option(names = "--timeout", description = "Redis command timeout (default: ${DEFAULT-VALUE}).", paramLabel = "<sec>")
     private long timeout = DEFAULT_TIMEOUT;
-    @Option(names = {"-n", "--db"}, description = "Database number (default: ${DEFAULT-VALUE})", paramLabel = "<int>")
+    @Option(names = {"-n", "--db"}, description = "Database number (default: ${DEFAULT-VALUE}).", paramLabel = "<db>")
     private int database = DEFAULT_DATABASE;
-    @Option(names = {"-c", "--cluster"}, description = "Enable cluster mode")
+    @Option(names = {"-c", "--cluster"}, description = "Enable cluster mode.")
     private boolean cluster;
-    @Option(names = {"-t", "--tls"}, description = "Establish a secure TLS connection")
+    @Option(names = "--tls", description = "Establish a secure TLS connection.")
     private boolean tls;
-    @Option(names = {"--no-verify-peer"}, description = "Do not verify peers when using TLS")
-    private boolean noVerifyPeer;
-    @Option(names = "--ks", description = "Path to keystore", paramLabel = "<file>", hidden = true)
+    @Option(names = "--no-verify-peer", description = "Verify peers when using TLS. True by default.", negatable = true)
+    private boolean verifyPeer = true;
+    @Option(names = "--ks", description = "Path to keystore.", paramLabel = "<file>", hidden = true)
     private File keystore;
-    @Option(names = "--ks-password", arity = "0..1", interactive = true, description = "Keystore password", paramLabel = "<pwd>", hidden = true)
+    @Option(names = "--ks-password", arity = "0..1", interactive = true, description = "Keystore password.", paramLabel = "<pwd>", hidden = true)
     private String keystorePassword;
-    @Option(names = "--ts", description = "Path to truststore", paramLabel = "<file>", hidden = true)
+    @Option(names = "--ts", description = "Path to truststore.", paramLabel = "<file>", hidden = true)
     private File truststore;
-    @Option(names = "--ts-password", arity = "0..1", interactive = true, description = "Truststore password", paramLabel = "<pwd>", hidden = true)
+    @Option(names = "--ts-password", arity = "0..1", interactive = true, description = "Truststore password.", paramLabel = "<pwd>", hidden = true)
     private String truststorePassword;
-    @Option(names = {"-l", "--latency"}, description = "Show latency metrics")
+    @Option(names = "--latency", description = "Show latency metrics.")
     private boolean showMetrics;
-    @Option(names = {"-m", "--pool"}, description = "Max pool connections (default: ${DEFAULT-VALUE})", paramLabel = "<int>")
+    @Option(names = "--pool-max", description = "Max pool connections (default: ${DEFAULT-VALUE}).", paramLabel = "<int>")
     private int poolMaxTotal = DEFAULT_POOL_MAX_TOTAL;
-    @Option(names = "--no-auto-reconnect", description = "Disable auto-reconnect", hidden = true)
-    private boolean noAutoReconnect;
-    @Option(names = "--client", description = "Client name used to connect to Redis")
+    @Option(names = "--no-auto-reconnect", description = "Auto reconnect on connection loss. True by default.", negatable = true, hidden = true)
+    private boolean autoReconnect = true;
+    @Option(names = "--client", description = "Client name used to connect to Redis.")
     private String clientName;
 
-    public RedisURI uri() {
-        RedisURI uri = redisURI;
-        if (uri == null) {
-            uri = new RedisURI();
+    public List<RedisURI> uris() {
+        List<RedisURI> uris = new ArrayList<>(this.uris);
+        if (uris.isEmpty()) {
+            RedisURI uri = new RedisURI();
             uri.setHost(host);
             uri.setPort(port);
             uri.setSocket(socket);
             uri.setSsl(tls);
+            uris.add(uri);
         }
-        if (noVerifyPeer) {
-            uri.setVerifyPeer(false);
+        for (RedisURI uri : uris) {
+            uri.setVerifyPeer(verifyPeer);
+            if (username != null) {
+                uri.setUsername(username);
+            }
+            if (password != null) {
+                uri.setPassword(password);
+            }
+            if (database != uri.getDatabase()) {
+                uri.setDatabase(database);
+            }
+            if (timeout != uri.getTimeout().getSeconds()) {
+                uri.setTimeout(Duration.ofSeconds(timeout));
+            }
+            if (clientName != null) {
+                uri.setClientName(clientName);
+            }
         }
-        if (username != null) {
-            uri.setUsername(username);
-        }
-        if (password != null) {
-            uri.setPassword(password);
-        }
-        if (database != uri.getDatabase()) {
-            uri.setDatabase(database);
-        }
-        if (timeout != uri.getTimeout().getSeconds()) {
-            uri.setTimeout(Duration.ofSeconds(timeout));
-        }
-        if (clientName != null) {
-            uri.setClientName(clientName);
-        }
-        return uri;
+        return uris;
     }
 
     private ClientResources clientResources() {
@@ -137,14 +140,14 @@ public class RedisOptions {
     }
 
     public RedisClient redisClient() {
-        RedisClient client = RedisClient.create(clientResources(), uri());
-        client.setOptions(ClientOptions.builder().autoReconnect(!noAutoReconnect).sslOptions(sslOptions()).build());
+        RedisClient client = RedisClient.create(clientResources(), uris().get(0));
+        client.setOptions(ClientOptions.builder().autoReconnect(autoReconnect).sslOptions(sslOptions()).build());
         return client;
     }
 
     public RedisClusterClient redisClusterClient() {
-        RedisClusterClient client = RedisClusterClient.create(clientResources(), uri());
-        client.setOptions(ClusterClientOptions.builder().autoReconnect(!noAutoReconnect).sslOptions(sslOptions()).build());
+        RedisClusterClient client = RedisClusterClient.create(clientResources(), uris());
+        client.setOptions(ClusterClientOptions.builder().autoReconnect(autoReconnect).sslOptions(sslOptions()).build());
         return client;
     }
 
