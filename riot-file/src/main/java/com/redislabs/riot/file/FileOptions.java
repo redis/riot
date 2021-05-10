44383a2e4460cf0a85b67b3c8b2bfde56def9f1b
jruaux <jruaux@gmail.com>
2021-05-09 22:55:54 -0700

    moved GCS and S3 related code to options classes

--- riot-file/src/main/java/com/redislabs/riot/file/FileOptions.java
+++ riot-file/src/main/java/com/redislabs/riot/file/FileOptions.java
@@ -1,20 +1,8 @@
 package com.redislabs.riot.file;
 
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.ServiceOptions;
-import com.google.cloud.storage.StorageOptions;
 import lombok.Data;
 import org.springframework.batch.item.resource.StandardInputResource;
 import org.springframework.batch.item.resource.StandardOutputResource;
-import org.springframework.cloud.aws.core.io.s3.SimpleStorageProtocolResolver;
-import org.springframework.cloud.gcp.autoconfigure.storage.GcpStorageAutoConfiguration;
-import org.springframework.cloud.gcp.core.GcpScope;
-import org.springframework.cloud.gcp.core.UserAgentHeaderProvider;
-import org.springframework.cloud.gcp.storage.GoogleStorageResource;
-import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.WritableResource;
@@ -23,11 +11,8 @@ import org.springframework.util.ResourceUtils;
 import picocli.CommandLine.ArgGroup;
 import picocli.CommandLine.Option;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.util.Base64;
 
 @Data
 public class FileOptions {
@@ -39,9 +24,9 @@ public class FileOptions {
     @Option(names = {"-z", "--gzip"}, description = "File is gzip compressed")
     private boolean gzip;
     @ArgGroup(exclusive = false, heading = "Amazon Simple Storage Service options%n")
-    private S3Options s3;
+    private S3Options s3 = new S3Options();
     @ArgGroup(exclusive = false, heading = "Google Cloud Storage options%n")
-    private GcsOptions gcs;
+    private GcsOptions gcs = new GcsOptions();
 
     @SuppressWarnings({"unchecked", "FieldCanBeLocal"})
     public static class FileOptionsBuilder<B extends FileOptionsBuilder<B>> {
@@ -106,53 +91,17 @@ public class FileOptions {
         return writable;
     }
 
-    public Resource s3Resource(String location) {
-        AmazonS3ClientBuilder clientBuilder = AmazonS3Client.builder();
-        if (s3 != null) {
-            if (s3.getRegion() != null) {
-                clientBuilder.withRegion(s3.getRegion());
-            }
-            if (s3.getAccessKey() != null) {
-                clientBuilder.withCredentials(new SimpleAWSCredentialsProvider(s3.getAccessKey(), s3.getSecretKey()));
-            }
-        }
-        SimpleStorageProtocolResolver resolver = new SimpleStorageProtocolResolver() {
-            @Override
-            public AmazonS3 getAmazonS3() {
-                return clientBuilder.build();
-            }
-        };
-        resolver.afterPropertiesSet();
-        return resolver.resolve(location, new DefaultResourceLoader());
-    }
-
     private Resource resource(String location, boolean readOnly) throws IOException {
         if (FileUtils.isS3(location)) {
-            return s3Resource(location);
+            return s3.resource(location);
         }
         if (FileUtils.isGcs(location)) {
-            return gcsResource(location, readOnly);
+            return gcs.resource(location, readOnly);
         }
         if (ResourceUtils.isUrl(location)) {
             return new UncustomizedUrlResource(location);
         }
         return new FileSystemResource(location);
-    }
-
-    private GoogleStorageResource gcsResource(String locationUri, boolean readOnly) throws IOException {
-        StorageOptions.Builder builder = StorageOptions.newBuilder().setProjectId(ServiceOptions.getDefaultProjectId()).setHeaderProvider(new UserAgentHeaderProvider(GcpStorageAutoConfiguration.class));
-        if (gcs != null) {
-            if (gcs.getCredentials() != null) {
-                builder.setCredentials(GoogleCredentials.fromStream(Files.newInputStream(gcs.getCredentials().toPath())).createScoped((readOnly ? GcpScope.STORAGE_READ_ONLY : GcpScope.STORAGE_READ_WRITE).getUrl()));
-            }
-            if (gcs.getEncodedKey() != null) {
-                builder.setCredentials(GoogleCredentials.fromStream(new ByteArrayInputStream(Base64.getDecoder().decode(gcs.getEncodedKey()))));
-            }
-            if (gcs.getProjectId() != null) {
-                builder.setProjectId(gcs.getProjectId());
-            }
-        }
-        return new GoogleStorageResource(builder.build().getService(), locationUri);
     }
 
 }
--- riot-file/src/main/java/com/redislabs/riot/file/GcsOptions.java
+++ riot-file/src/main/java/com/redislabs/riot/file/GcsOptions.java
@@ -1,9 +1,20 @@
 package com.redislabs.riot.file;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.ServiceOptions;
+import com.google.cloud.storage.StorageOptions;
 import lombok.Data;
+import org.springframework.cloud.gcp.autoconfigure.storage.GcpStorageAutoConfiguration;
+import org.springframework.cloud.gcp.core.GcpScope;
+import org.springframework.cloud.gcp.core.UserAgentHeaderProvider;
+import org.springframework.cloud.gcp.storage.GoogleStorageResource;
 import picocli.CommandLine.Option;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Base64;
 
 @Data
 public class GcsOptions {
@@ -14,5 +25,19 @@ public class GcsOptions {
     private String projectId;
     @Option(names = "--gcs-key", arity = "0..1", interactive = true, description = "GCS Base64 encoded key", paramLabel = "<key>")
     private String encodedKey;
+
+    public GoogleStorageResource resource(String locationUri, boolean readOnly) throws IOException {
+        StorageOptions.Builder builder = StorageOptions.newBuilder().setProjectId(ServiceOptions.getDefaultProjectId()).setHeaderProvider(new UserAgentHeaderProvider(GcpStorageAutoConfiguration.class));
+        if (credentials != null) {
+            builder.setCredentials(GoogleCredentials.fromStream(Files.newInputStream(credentials.toPath())).createScoped((readOnly ? GcpScope.STORAGE_READ_ONLY : GcpScope.STORAGE_READ_WRITE).getUrl()));
+        }
+        if (encodedKey != null) {
+            builder.setCredentials(GoogleCredentials.fromStream(new ByteArrayInputStream(Base64.getDecoder().decode(encodedKey))));
+        }
+        if (projectId != null) {
+            builder.setProjectId(projectId);
+        }
+        return new GoogleStorageResource(builder.build().getService(), locationUri);
+    }
 
 }
