4e62bf592d3f33295add8e54c3edbb2a0dcc7049
Julien Ruaux <jruaux@gmail.com>
2021-12-04 19:21:28 -0800

    Fixed reading MANIFEST.MF for version info

--- core/riot-core/src/main/java/com/redis/riot/ManifestVersionProvider.java
+++ core/riot-core/src/main/java/com/redis/riot/ManifestVersionProvider.java
@@ -1,23 +1,23 @@
 package com.redis.riot;
 
-import picocli.CommandLine;
-
 import java.io.IOException;
 import java.net.URL;
 import java.util.Enumeration;
 import java.util.jar.Attributes;
 import java.util.jar.Manifest;
 
+import picocli.CommandLine;
+
 /**
- * {@link picocli.CommandLine.IVersionProvider} implementation that returns version information from
- * the jar file's {@code /META-INF/MANIFEST.MF} file.
+ * {@link picocli.CommandLine.IVersionProvider} implementation that returns
+ * version information from the jar file's {@code /META-INF/MANIFEST.MF} file.
  */
 public class ManifestVersionProvider implements CommandLine.IVersionProvider {
 
-    @Override
-    public String[] getVersion() {
-        return new String[]{
-                // @formatter:off
+	@Override
+	public String[] getVersion() throws Exception {
+		return new String[] {
+				// @formatter:off
                 "",
                 "      ▀        █     @|fg(4;1;1) ██████████████████████████|@",
                 " █ ██ █  ███  ████   @|fg(4;2;1) ██████████████████████████|@",
@@ -26,31 +26,32 @@ public class ManifestVersionProvider implements CommandLine.IVersionProvider {
                 " █    █  ███    ██   @|fg(0;3;4) ██████████████████████████|@"+ "  v" + getVersionString(),
                 ""};
                 // @formatter:on
-    }
+	}
 
-    private String getVersionString() {
-        try {
-            Enumeration<URL> resources = getClass().getClassLoader().getResources("META-INF/MANIFEST.MF");
-            while (resources.hasMoreElements()) {
-                URL url = resources.nextElement();
-                Manifest manifest = new Manifest(url.openStream());
-                if (isApplicableManifest(manifest)) {
-                    Attributes attr = manifest.getMainAttributes();
-                    return String.valueOf(get(attr, "Implementation-Version"));
-                }
-            }
-        } catch (IOException ex) {
-            // ignore
-        }
-        return "N/A";
-    }
+	private String getVersionString() throws IOException {
+		Enumeration<URL> resources = ManifestVersionProvider.class.getClassLoader()
+				.getResources("META-INF/MANIFEST.MF");
+		while (resources.hasMoreElements()) {
+			URL url = resources.nextElement();
+			try {
+				Manifest manifest = new Manifest(url.openStream());
+				if (isApplicableManifest(manifest)) {
+					Attributes attr = manifest.getMainAttributes();
+					return String.valueOf(get(attr, "Implementation-Version"));
+				}
+			} catch (IOException ex) {
+				return "Unable to read from " + url + ": " + ex;
+			}
+		}
+		return "N/A";
+	}
 
-    private boolean isApplicableManifest(Manifest manifest) {
-        Attributes attributes = manifest.getMainAttributes();
-        return "RIOT".equals(get(attributes, "Implementation-Title"));
-    }
+	private boolean isApplicableManifest(Manifest manifest) {
+		Attributes attributes = manifest.getMainAttributes();
+		return "RIOT".equals(get(attributes, "Implementation-Title"));
+	}
 
-    private static Object get(Attributes attributes, String key) {
-        return attributes.get(new Attributes.Name(key));
-    }
+	private static Object get(Attributes attributes, String key) {
+		return attributes.get(new Attributes.Name(key));
+	}
 }
