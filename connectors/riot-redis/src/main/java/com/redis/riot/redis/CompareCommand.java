018958e3e4a2c8fa71598ad8ba57f9acf18c55a0
julien <julien@c64.arcillaux.lan>
2022-02-25 14:24:51 -0800

    Updated documentation

--- connectors/riot-redis/src/main/java/com/redis/riot/redis/CompareCommand.java
+++ connectors/riot-redis/src/main/java/com/redis/riot/redis/CompareCommand.java
@@ -5,7 +5,7 @@ import org.springframework.batch.core.job.builder.JobBuilder;
 
 import picocli.CommandLine.Command;
 
-@Command(name = "compare", description = "Compare a target Redis database with a source Redis database and prints the differences")
+@Command(name = "compare", description = "Compare 2 Redis databases and print the differences")
 public class CompareCommand extends AbstractTargetCommand {
 
 	@Override
