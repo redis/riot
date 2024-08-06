09fe2358b903358b701cad3935bf2f3a2a96a571
jruaux <jruaux@gmail.com>
2024-08-06 16:53:22 -0700

    feat: Report exceptions without transformation

--- core/riot-core/src/main/java/com/redis/riot/core/PrintExceptionMessageHandler.java
+++ core/riot-core/src/main/java/com/redis/riot/core/PrintExceptionMessageHandler.java
@@ -1,9 +1,5 @@
 package com.redis.riot.core;
 
-import java.util.concurrent.ExecutionException;
-
-import org.springframework.batch.core.step.skip.SkipException;
-
 import picocli.CommandLine;
 import picocli.CommandLine.IExecutionExceptionHandler;
 import picocli.CommandLine.ParseResult;
@@ -12,31 +8,17 @@ public class PrintExceptionMessageHandler implements IExecutionExceptionHandler 
 
 	public int handleExecutionException(Exception ex, CommandLine cmd, ParseResult parseResult) {
 
-		boolean stacktrace = false;
-
 		if (cmd.getCommand() instanceof AbstractCommand) {
-			stacktrace = ((AbstractCommand<?>) cmd.getCommand()).getLoggingArgs().isStacktrace();
+			if (((AbstractCommand<?>) cmd.getCommand()).getLoggingArgs().isStacktrace()) {
+				ex.printStackTrace(cmd.getErr());
+			}
 		}
 
-		if (stacktrace) {
-			ex.printStackTrace(cmd.getErr());
-		}
-
-		Throwable rootCause = rootCause(ex);
 		// bold red error message
-		cmd.getErr().println(cmd.getColorScheme().errorText(rootCause.getMessage()));
+		cmd.getErr().println(cmd.getColorScheme().errorText(ex.getMessage()));
 
-		return cmd.getExitCodeExceptionMapper() != null ? cmd.getExitCodeExceptionMapper().getExitCode(rootCause)
+		return cmd.getExitCodeExceptionMapper() != null ? cmd.getExitCodeExceptionMapper().getExitCode(ex)
 				: cmd.getCommandSpec().exitCodeOnExecutionException();
 	}
 
-	private Throwable rootCause(Throwable ex) {
-		if (ex instanceof SkipException) {
-			return rootCause(((SkipException) ex).getCause());
-		}
-		if (ex instanceof ExecutionException) {
-			return ((ExecutionException) ex).getCause();
-		}
-		return ex;
-	}
 }
