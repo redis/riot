7b7a06ee6e2c28cfd76aa929d60debe131900a7d
Richard Boldiš <richard@boldis.dev>
2024-09-14 21:11:50 +0200

    feat: Add KeyValue and EvaluationContext args to compare

--- plugins/riot/src/main/java/com/redis/riot/Compare.java
+++ plugins/riot/src/main/java/com/redis/riot/Compare.java
@@ -1,7 +1,19 @@
 package com.redis.riot;
 
-import org.springframework.batch.core.Job;
+import com.redis.riot.core.RiotUtils;
+import com.redis.riot.function.StringKeyValue;
+import com.redis.riot.function.ToStringKeyValue;
+import com.redis.spring.batch.item.redis.common.KeyValue;
+import com.redis.spring.batch.item.redis.reader.KeyComparisonItemReader;
 
+import io.lettuce.core.codec.ByteArrayCodec;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.function.FunctionItemProcessor;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
+
+import picocli.CommandLine.ArgGroup;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
@@ -13,6 +25,12 @@ public class Compare extends AbstractCompareCommand {
 
 	@Option(names = "--quick", description = "Skip value comparison.")
 	private boolean quick;
+
+	@ArgGroup(exclusive = false)
+	private final EvaluationContextArgs evaluationContextArgs = new EvaluationContextArgs();
+
+	@ArgGroup(exclusive = false, heading = "Processor options%n")
+	private final KeyValueProcessorArgs processorArgs = new KeyValueProcessorArgs();
 
 	@Override
 	protected boolean isQuickCompare() {
@@ -27,6 +45,37 @@ public class Compare extends AbstractCompareCommand {
 	@Override
 	protected Job job() {
 		return job(compareStep());
+	}
+
+	private StandardEvaluationContext evaluationContext() {
+		log.info("Creating SpEL evaluation context with {}", evaluationContextArgs);
+		StandardEvaluationContext evaluationContext = evaluationContextArgs.evaluationContext();
+		configure(evaluationContext);
+		return evaluationContext;
+	}
+
+	private ItemProcessor<KeyValue<byte[], Object>, KeyValue<byte[], Object>> keyValueProcessor() {
+		StandardEvaluationContext evaluationContext = evaluationContext();
+		log.info("Creating processor with {}", processorArgs);
+		ItemProcessor<KeyValue<String, Object>, KeyValue<String, Object>> processor = processorArgs
+				.processor(evaluationContext);
+		if (processor == null) {
+			return null;
+		}
+		ToStringKeyValue<byte[]> code = new ToStringKeyValue<>(ByteArrayCodec.INSTANCE);
+		StringKeyValue<byte[]> decode = new StringKeyValue<>(ByteArrayCodec.INSTANCE);
+		return RiotUtils.processor(new FunctionItemProcessor<>(code), processor, new FunctionItemProcessor<>(decode));
+	}
+
+	private ItemProcessor<KeyValue<byte[], Object>, KeyValue<byte[], Object>> processor() {
+		return RiotUtils.processor(new KeyValueFilter<>(ByteArrayCodec.INSTANCE, log), keyValueProcessor());
+	}
+
+	@Override
+	protected KeyComparisonItemReader<byte[], byte[]> compareReader() {
+		KeyComparisonItemReader<byte[], byte[]> reader = super.compareReader();
+		reader.setProcessor(processor());
+		return reader;
 	}
 
 	public boolean isCompareStreamMessageId() {
