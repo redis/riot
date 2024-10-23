02e2859da37317e783d81ca44b7f527ce5317816
jruaux <jruaux@gmail.com>
2024-10-23 12:24:33 -0700

    test: Using fixed tags for stack and enterprise Redis containers

--- plugins/riot/src/test/java/com/redis/riot/RedisContainerFactory.java
+++ plugins/riot/src/test/java/com/redis/riot/RedisContainerFactory.java
@@ -6,24 +6,23 @@ import com.redis.enterprise.testcontainers.RedisEnterpriseContainer;
 import com.redis.enterprise.testcontainers.RedisEnterpriseServer;
 import com.redis.testcontainers.RedisStackContainer;
 
-public class RedisContainerFactory {
+public interface RedisContainerFactory {
 
-	private RedisContainerFactory() {
-	}
+	String ENTERPRISE_TAG = "7.4.6-102";
+	String STACK_TAG = "7.2.0-v13";
 
-	public static RedisStackContainer stack() {
-		return new RedisStackContainer(RedisStackContainer.DEFAULT_IMAGE_NAME.withTag("7.2.0-v11"));
+	static RedisStackContainer stack() {
+		return new RedisStackContainer(RedisStackContainer.DEFAULT_IMAGE_NAME.withTag(STACK_TAG));
 	}
 
 	@SuppressWarnings("resource")
-	public static RedisEnterpriseContainer enterprise() {
-		return new RedisEnterpriseContainer(
-				RedisEnterpriseContainer.DEFAULT_IMAGE_NAME.withTag(RedisEnterpriseContainer.DEFAULT_TAG))
+	static RedisEnterpriseContainer enterprise() {
+		return new RedisEnterpriseContainer(RedisEnterpriseContainer.DEFAULT_IMAGE_NAME.withTag(ENTERPRISE_TAG))
 				.withDatabase(Database.builder().name("BatchTests").memoryMB(50).ossCluster(true)
 						.modules(RedisModule.TIMESERIES, RedisModule.JSON, RedisModule.SEARCH).build());
 	}
 
-	public static RedisEnterpriseServer enterpriseServer() {
+	static RedisEnterpriseServer enterpriseServer() {
 		RedisEnterpriseServer server = new RedisEnterpriseServer();
 		server.withDatabase(Database.builder().shardCount(2).port(12001).ossCluster(true)
 				.modules(RedisModule.JSON, RedisModule.SEARCH, RedisModule.TIMESERIES).build());
