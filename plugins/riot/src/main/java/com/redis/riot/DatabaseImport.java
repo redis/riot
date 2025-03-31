13d37608d6584b3a3c31a7bd965f8d6ea4fa0958
julien <julien@mac>
2025-03-31 14:56:10 -0700

    refactor: Use JDBC reader factory in db import command

--- plugins/riot/src/main/java/com/redis/riot/DatabaseImport.java
+++ plugins/riot/src/main/java/com/redis/riot/DatabaseImport.java
@@ -2,15 +2,8 @@ package com.redis.riot;
 
 import java.util.Map;
 
-import javax.sql.DataSource;
-
 import org.springframework.batch.core.Job;
-import org.springframework.batch.item.database.JdbcCursorItemReader;
 import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
-import org.springframework.jdbc.core.ColumnMapRowMapper;
-import org.springframework.util.Assert;
-
-import com.redis.riot.core.RiotException;
 
 import picocli.CommandLine.ArgGroup;
 import picocli.CommandLine.Command;
@@ -30,34 +23,10 @@ public class DatabaseImport extends AbstractRedisImportCommand {
 
 	@Override
 	protected Job job() {
-		return job(step(reader()));
-	}
-
-	protected JdbcCursorItemReader<Map<String, Object>> reader() {
-		Assert.hasLength(sql, "No SQL statement specified");
-		log.info("Creating data source with {}", dataSourceArgs);
-		DataSource dataSource;
-		try {
-			dataSource = dataSourceArgs.dataSource();
-		} catch (Exception e) {
-			throw new RiotException(e);
-		}
-		log.info("Creating JDBC reader with sql=\"{}\" {}", sql, readerArgs);
-		JdbcCursorItemReaderBuilder<Map<String, Object>> reader = new JdbcCursorItemReaderBuilder<>();
-		reader.dataSource(dataSource);
-		reader.sql(sql);
-		reader.saveState(false);
-		reader.rowMapper(new ColumnMapRowMapper());
-		reader.fetchSize(readerArgs.getFetchSize());
-		reader.maxRows(readerArgs.getMaxRows());
-		reader.queryTimeout(Math.toIntExact(readerArgs.getQueryTimeout().getValue().toMillis()));
-		reader.useSharedExtendedConnection(readerArgs.isUseSharedExtendedConnection());
-		reader.verifyCursorPosition(readerArgs.isVerifyCursorPosition());
-		if (readerArgs.getMaxItemCount() > 0) {
-			reader.maxItemCount(readerArgs.getMaxItemCount());
-		}
-		reader.name(sql);
-		return reader.build();
+		log.info("Creating JDBC reader with sql=\"{}\" {} {}", sql, dataSourceArgs, readerArgs);
+		JdbcCursorItemReaderBuilder<Map<String, Object>> reader = JdbcCursorItemReaderFactory.create(sql,
+				dataSourceArgs, readerArgs);
+		return job(step(reader.build()));
 	}
 
 	public String getSql() {
